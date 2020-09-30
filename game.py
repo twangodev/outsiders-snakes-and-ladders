4ac8b7317f8ee9e80ce440e05766f555eee46f14
James Ding <twango@users.noreply.github.com>
2020-09-30 12:38:57 -0700

    updates

--- game.py
+++ game.py
@@ -1,2 +1,12 @@
 import pygame
+import json
 
+with open('config.json') as configuration: #Read JSON from config.json
+    config = json.loads(configuration.read())
+
+#Initialize pygame
+pygame.init()
+
+# Create Window
+gameDisplay = pygame.display.set_mode((config["display"]["width"], config["display"]["height"]))
+pygame.display.set_caption(config["display"]["caption"])
