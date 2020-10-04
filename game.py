4f131b91c417e4ee7591f8d12e49956ffe64e392
James Ding <twango@users.noreply.github.com>
2020-10-04 08:53:30 -0700

    final code commit

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ pygame.display.set_caption(config["display"]["caption"])
 #Load Images
 startingbg = pygame.image.load("./assets/startingbg.png")
 startbtn = pygame.image.load("./assets/startbtn.png")
-grid = pygame.image.load("./assets/grid.png")
+grid = pygame.image.load("./assets/grid-snakes.png")
 snake = pygame.image.load("./assets/snake.png")
 banner = pygame.image.load("./assets/announcement.png")
 checkmark = pygame.image.load("./assets/check.png")
@@ -69,133 +69,6 @@ def correct_checker(id, ug_answer):
             return True
         else:
             return False
-
-#Main Modules
-
-def start():
-    gameDisplay.fill([42, 42, 42])
-    loop = True
-    while(loop == True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = False
-                print("Game Quit")
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.pos[0] >= centercoord(30,100)[0] and event.pos[0] <= (centercoord(30,100)[0]+100) and event.pos[1] >= centercoord(30,100)[1] and event.pos[1] <= (centercoord(30,100)[1]+30):
-                    mainscreen()
-                    loop = False
-        gameDisplay.blit(startingbg, [0,0]) #Set Background
-        gameDisplay.blit(startbtn, centercoord(30, 100))
-        pygame.display.update()
-        clock.tick(config["display"]["fps"])
-        pygame.display.flip()
-
-def mainscreen():
-    id_randomized = []
-    for x in config["questions"].keys():
-        id_randomized.append(x)
-    random.shuffle(id_randomized)
-    print("Question Order: {}".format(id_randomized))
-
-    #Private Variables
-    start_time = datetime.datetime.now()
-    answer = "" 
-    provided_answer = ""
-    offset = [config["snake-xoffset"], 0]
-    position = [0,0]
-    loop = True
-    diceface = dice[0]
-    displaycube = False
-    bannerdisplay = False
-    answer_text = ""
-    stoprolling = True
-    collectbannerbuttons = False
-    while(loop == True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = False
-                print("Game Quit")
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    answer = answer[:-1]
-                elif event.key == pygame.K_RETURN:
-                    provided_answer = answer
-                    answer = ""
-                    bannerdisplay = True
-                    stoprolling = False
-                else:
-                    letter = str(keystroke_recorder(event))
-                    answer = answer + letter #Adding Letters
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if collectbannerbuttons == True:
-                    if event.pos[0] >= int(config["display"]["width"]/2-180) and event.pos[0] <= int((config["display"]["width"]/2-180)+100) and event.pos[1] >= int(config["display"]["height"]/2+40) and event.pos[1] <= int((config["display"]["height"]/2+40)+30):
-                        loop = False
-                    if event.pos[0] >= int(config["display"]["width"]/2-50) and event.pos[0] <= int(int(config["display"]["width"]/2-50)+200) and event.pos[1] >= int(config["display"]["height"]/2+10) and event.pos[1] <= int(int(config["display"]["height"]/2+40)+30):
-                        bannerdisplay = False
-                        id_randomized.pop(0)
-        #Display
-        gameDisplay.fill([255, 255, 255])
-        gameDisplay.blit(grid, [0,0]) #Set Background
-        #Score Update
-        score = str(position[0] + (position[1]*10) + 1)
-        score_text = font.render("Score: " + score, True, (0,0,0), (255,255,255))
-        gameDisplay.blit(score_text, [0,0])
-        #Time
-        time_elapsed = str(round((datetime.datetime.now() - start_time).total_seconds()))
-        time_text = font.render("Seconds Elapsed: " + time_elapsed, True, (0,0,0), (255,255,255))
-        gameDisplay.blit(time_text, [0, config["display"]["height"]-20])
-        #Snake
-        gameDisplay.blit(snake, offset)
-        #Question
-        try:
-            question = config["questions"][id_randomized[0]]
-        except Exception:
-            print("Out of questions.")
-            loop = False
-        question_text = font.render("Question: " + question, True, (0,0,0), (255,255,255))
-        gameDisplay.blit(question_text, [0, config["display"]["height"]-40])
-        #Answer Text Update
-        answer_text = font.render("Your Answer: " + str(answer), True, (0,0,0), (255,255,255)) #Answer
-        gameDisplay.blit(answer_text, [int((config["display"]["width"]/2)-200), int(config["display"]["height"]-20)])
-        #Announcments
-        if bannerdisplay == True:
-            provided_answer_text = font.render("Your Answer: " + provided_answer, True, (0,0,0))
-            gameDisplay.blit(banner, centercoord(341, 604))
-            gameDisplay.blit(provided_answer_text, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-75)])
-            if correct_checker(id_randomized[0], provided_answer) == True:
-                gameDisplay.blit(checkmark, [int(config["display"]["width"]/2), int(config["display"]["height"]/2-70)])
-                #If Correct Move the Snake
-                displaycube = True
-                if stoprolling == False:
-                    randomnumber = random.randint(1,6)
-                    diceface = dice[randomnumber-1]
-                    print("Random Number: " + str(randomnumber))
-                    for x in range(randomnumber):
-                        offset = move(offset)
-                        position = offset_to_pos(offset)
-                    stoprolling = True
-                rolled_text = font.render("You rolled a " + str(randomnumber), True, (0,0,0)) #Rolled Text
-                gameDisplay.blit(rolled_text, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-30)])
-                #Render Buttons
-                collectbannerbuttons = True
-                gameDisplay.blit(quitbtn, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2+40)])
-                gameDisplay.blit(continuebtn, [int(config["display"]["width"]/2-80), int(config["display"]["height"]/2+40)])
-            elif correct_checker(id_randomized[0], provided_answer) == False:
-                gameDisplay.blit(xmark, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-50)])
-                displaycube = False
-                #Render Buttons
-                collectbannerbuttons = True
-                gameDisplay.blit(quitbtn, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2+40)])
-                gameDisplay.blit(continuebtn, [int(config["display"]["width"]/2-50), int(config["display"]["height"]/2+40)])
-        else:
-            collectbannerbuttons = False
-        if displaycube == True:
-            previous_roll_text = font.render("Previous Roll:", True, (0,0,0))
-            gameDisplay.blit(previous_roll_text, [0, 40])
-            gameDisplay.blit(diceface, [0, 60])
-        clock.tick(config["display"]["fps"])
-        pygame.display.update()
-        pygame.display.flip()
 
 def keystroke_recorder(event):
     if event.type == pygame.KEYDOWN:
@@ -280,5 +153,174 @@ def keystroke_recorder(event):
             return " "
         else:
             return ""
+
+def snakes_and_ladders(position, offset):
+    
+    #Snakes
+    alert_text = font.render("You discovered and took a snake!", True, (0,0,0))
+    if position == [6-1, 0]:
+        newposition = [6-1, 4]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    elif position == [3-1, 1]:
+        newposition = [9-1, 6]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    elif position == [2-1, 4]:
+        newposition = [2-1, 8]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    #Ladders
+    alert_text = font.render("You discovered and took a ladder!", True, (0,0,0))
+    if position == [1-1, 3]:
+        newposition = [2-1, 0]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    elif position == [10-1, 5]:
+        newposition = [9-1, 2]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    elif position == [4-1, 7]:
+        newposition = [5-1, 4]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    elif position == [9-1, 9]:
+        newposition = [8-1, 6]
+        newoffset = [(newposition[0]*60)+config["snake-xoffset"], newposition[1]*60]
+        return newposition, newoffset, alert_text
+    else:
+        alert_text = None
+        return position, offset, alert_text
+#Main Modules
+
+def start():
+    gameDisplay.fill([42, 42, 42])
+    loop = True
+    while(loop == True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+                print("Game Quit")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] >= centercoord(30,100)[0] and event.pos[0] <= (centercoord(30,100)[0]+100) and event.pos[1] >= centercoord(30,100)[1] and event.pos[1] <= (centercoord(30,100)[1]+30):
+                    mainscreen()
+                    loop = False
+        gameDisplay.blit(startingbg, [0,0]) #Set Background
+        gameDisplay.blit(startbtn, centercoord(30, 100))
+        pygame.display.update()
+        clock.tick(config["display"]["fps"])
+        pygame.display.flip()
+
+def mainscreen():
+    id_randomized = []
+    for x in config["questions"].keys():
+        id_randomized.append(x)
+    random.shuffle(id_randomized)
+    print("Question Order: {}".format(id_randomized))
+
+    #Private Variables
+    start_time = datetime.datetime.now()
+    answer = "" 
+    provided_answer = ""
+    answer_text = ""
+    offset = [config["snake-xoffset"], 0]
+    position = [0,0]
+    diceface = dice[0]
+    loop = True #Boolean Time LOL
+    displaycube = False
+    bannerdisplay = False
+    stoprolling = True
+    collectbannerbuttons = False
+    while loop == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                loop = False
+                print("Game Quit")
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    answer = answer[:-1]
+                elif event.key == pygame.K_RETURN:
+                    provided_answer = answer
+                    answer = ""
+                    bannerdisplay = True
+                    stoprolling = False
+                else:
+                    letter = str(keystroke_recorder(event))
+                    answer = answer + letter #Adding Letters
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if collectbannerbuttons == True:
+                    if event.pos[0] >= int(config["display"]["width"]/2-180) and event.pos[0] <= int((config["display"]["width"]/2-180)+100) and event.pos[1] >= int(config["display"]["height"]/2+40) and event.pos[1] <= int((config["display"]["height"]/2+40)+30):
+                        loop = False
+                    if event.pos[0] >= int(config["display"]["width"]/2-50) and event.pos[0] <= int(int(config["display"]["width"]/2-50)+200) and event.pos[1] >= int(config["display"]["height"]/2+10) and event.pos[1] <= int(int(config["display"]["height"]/2+40)+30):
+                        bannerdisplay = False
+                        id_randomized.pop(0)
+        #Display
+        gameDisplay.fill([255, 255, 255])
+        gameDisplay.blit(grid, [0,0]) #Set Background
+        #Score Update
+        score = str(position[0] + (position[1]*10) + 1)
+        score_text = font.render("Score: " + score, True, (0,0,0), (255,255,255))
+        gameDisplay.blit(score_text, [0,0])
+        #Time
+        time_elapsed = str(round((datetime.datetime.now() - start_time).total_seconds()))
+        time_text = font.render("Seconds Elapsed: " + time_elapsed, True, (0,0,0), (255,255,255))
+        gameDisplay.blit(time_text, [0, config["display"]["height"]-20])
+        #Snake
+        gameDisplay.blit(snake, offset)
+        #Question
+        try:
+            question = config["questions"][id_randomized[0]]
+        except Exception:
+            print("Out of questions.")
+            loop = False
+        question_text = font.render("Question: " + question, True, (0,0,0), (255,255,255))
+        gameDisplay.blit(question_text, [0, config["display"]["height"]-40])
+        #Answer Text Update
+        answer_text = font.render("Your Answer: " + str(answer), True, (0,0,0), (255,255,255)) #Answer
+        gameDisplay.blit(answer_text, [int((config["display"]["width"]/2)-200), int(config["display"]["height"]-20)])
+        #Announcments
+        if bannerdisplay == True:
+            provided_answer_text = font.render("Your Answer: " + provided_answer, True, (0,0,0))
+            gameDisplay.blit(banner, centercoord(341, 604))
+            gameDisplay.blit(provided_answer_text, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-75)])
+            if correct_checker(id_randomized[0], provided_answer) == True:
+                gameDisplay.blit(checkmark, [int(config["display"]["width"]/2), int(config["display"]["height"]/2-70)])
+                #If Correct Move the Snake
+                displaycube = True
+                if stoprolling == False:
+                    randomnumber = random.randint(1,6)
+                    diceface = dice[randomnumber-1]
+                    print("Random Number: " + str(randomnumber))
+                    for x in range(randomnumber):
+                        offset = move(offset)
+                        position = offset_to_pos(offset)
+                    position, offset, alert_text = snakes_and_ladders(position, offset)
+                    stoprolling = True
+                rolled_text = font.render("You rolled a " + str(randomnumber), True, (0,0,0)) #Rolled Text
+                gameDisplay.blit(rolled_text, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-30)])
+                #Snake Ladder Checker
+                if alert_text != None:
+                    gameDisplay.blit(alert_text, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2)])
+                #Render Buttons
+                collectbannerbuttons = True
+                gameDisplay.blit(quitbtn, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2+40)])
+                gameDisplay.blit(continuebtn, [int(config["display"]["width"]/2-80), int(config["display"]["height"]/2+40)])
+                    
+            elif correct_checker(id_randomized[0], provided_answer) == False:
+                gameDisplay.blit(xmark, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2-50)])
+                displaycube = False
+                #Render Buttons
+                collectbannerbuttons = True
+                gameDisplay.blit(quitbtn, [int(config["display"]["width"]/2-180), int(config["display"]["height"]/2+40)])
+                gameDisplay.blit(continuebtn, [int(config["display"]["width"]/2-50), int(config["display"]["height"]/2+40)])
+        else:
+            collectbannerbuttons = False
+        if displaycube == True:
+            previous_roll_text = font.render("Previous Roll:", True, (0,0,0))
+            gameDisplay.blit(previous_roll_text, [0, 40])
+            gameDisplay.blit(diceface, [0, 60])
+        clock.tick(config["display"]["fps"])
+        pygame.display.update()
+        pygame.display.flip()
 
 start()
